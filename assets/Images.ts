e7351f9eb7271324cefaf53db1c8a63ca72db64b
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-05-19 11:53:18 +0700

    fix: :alien: Fix image api

--- assets/Images.ts
+++ assets/Images.ts
@@ -1,18 +1,18 @@
 const cdn = process.env.NEXT_PUBLIC_CDN_API;
-const cdnEnhance = cdn + "/e_enhance/Serenity";
+const cdnEnhance = cdn + "/e_enhance";
 
-const rawCoverImage = `${cdnEnhance}/ankrqsdcl9dlyiwo4wed.jpg`;
-const rawLibrary1 = `${cdnEnhance}/pcb3ivpv7fz49ve0ekfy.jpg`;
-const rawLibrary2 = `${cdnEnhance}/uxrvxhq6sa97mglgl7kh.jpg`;
-const rawImage1 = `${cdnEnhance}/gjp9zfphuopcgweu9gxs.jpg`;
+const rawCoverImage = `${cdnEnhance}/e_gen_restore/Serenity/ankrqsdcl9dlyiwo4wed.jpg`;
+const rawLibrary1 = `${cdnEnhance}/Serenity/pcb3ivpv7fz49ve0ekfy.jpg`;
+const rawLibrary2 = `${cdnEnhance}/Serenity/uxrvxhq6sa97mglgl7kh.jpg`;
+const rawImage1 = `${cdnEnhance}/Serenity/gjp9zfphuopcgweu9gxs.jpg`;
 const rawTheApp = `${cdn}/Serenity/jqbnquroruwrx8076pzu.jpg`;
 const rawAppDev = `${cdn}/Serenity/itdsuw5vplnsarevr08y.jpg`;
 const rawAudioSpes = `${cdn}/Serenity/xjdbrtcrrjmhcdujniiu.jpg`;
 const rawMusicCur = `${cdn}/Serenity/qldpsru6seorrqfawczq.jpg`;
-const rawTheTeam = `${cdnEnhance}/p0tof3wa77wxeq4adjyd.jpg`;
-const rawMusicShare = `${cdnEnhance}/q6fnewnmfqvoq6pzkp7z.jpg`;
-const rawWalking = `${cdnEnhance}/rlkd9ldfuhwg3s1naw9k.jpg`;
-const rawSubscribe = `${cdnEnhance}/motytlv8ar0gofxme99p.jpg`;
+const rawTheTeam = `${cdnEnhance}/Serenity/p0tof3wa77wxeq4adjyd.jpg`;
+const rawMusicShare = `${cdnEnhance}/Serenity/q6fnewnmfqvoq6pzkp7z.jpg`;
+const rawWalking = `${cdnEnhance}/Serenity/rlkd9ldfuhwg3s1naw9k.jpg`;
+const rawSubscribe = `${cdnEnhance}/Serenity/motytlv8ar0gofxme99p.jpg`;
 const rawSerenityLogo = `${cdn}/e_improve:outdoor/Serenity/zuehemfwgnjitliavvpf.jpg`;
 const rawSerenityLogoBig = `${cdn}/e_improve:outdoor/Serenity/qdx0tz6myaesmso1howm.jpg`;
 
